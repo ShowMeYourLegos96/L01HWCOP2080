6e14eefca7a3f90016966fa51255947689c3c3db
Christopher Ponds <87038488+ShowMeYourLegos96@users.noreply.github.com>
2026-01-17 21:23:34 +0000

    Testing the print function

---
+++ print_test.py
@@ -0,0 +1,9 @@
+my_name = "Christopher Ponds"
+print(my_name)
+#Edit your code part 4 
+sen_one = "Use 4 spaces per indentation level"
+print(sen_one)
+#Edit your code part 5
+print("Statements cannot cross logical line boundaries except where NEWLINE is allowed by the syntax")
+#Edit your code part 6
+print("I hope to have a better understanding of Python. I hope to use more of my github account. I hope to solve complex problems using python")
